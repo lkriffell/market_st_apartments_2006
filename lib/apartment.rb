d2e4af3be5ad52ced5fd5b0c60af50f262b99ac1
Logan Riffell <lkriffell@gmail.com>
2020-07-14 09:15:31 -0600

    Class passes first test

---
+++ lib/apartment.rb
@@ -0,0 +1,10 @@
+class Apartment
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
+  def initialize(apt = {})
+    @number = apt[:number]
+    @monthly_rent = apt[:monthly_rent]
+    @bathrooms = apt[:bathrooms]
+    @bedrooms = apt[:bedrooms]
+    @renter = apt[:renter]
+  end
+end

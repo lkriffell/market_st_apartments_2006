5832a49bef193fe72b393a90bf370413331f80cf
Logan Riffell <lkriffell@gmail.com>
2020-07-14 10:02:02 -0600

    Add test for rented_units

--- test/building_test.rb
+++ test/building_test.rb
@@ -37,5 +37,24 @@ class BuildingTest < Minitest::Test
     assert_equal 1099.5 , building.average_rent
   end
 
+  def test_rented_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    unit2.add_renter(renter1)
+    require "pry"; binding.pry
+
+    assert_equal [unit2], building.rented_units
+
+
+
+  end
+
 
 end

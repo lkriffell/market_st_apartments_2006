e5415a191aae52d3306d4638bfc5a9dce9919cc8
Logan Riffell <lkriffell@gmail.com>
2020-07-14 09:06:42 -0600

    Add renter class

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -1,6 +1,6 @@
 require './lib/renter'
+require 'minitest/autorun'
 require 'minitest/pride'
-require 'minitest/autotest'
 class RenterTest < Minitest::Test
   def test_renter_has_name
     renter1 = Renter.new("Jessie")

3693ead8c7b6cb789d4810a1586a5fb87f880123
Logan Riffell <lkriffell@gmail.com>
2020-07-14 09:45:43 -0600

    Add passing renters method

--- lib/building.rb
+++ lib/building.rb
@@ -1,11 +1,19 @@
 class Building
-  attr_reader :units
+  attr_reader :units, :renters
 
   def initialize()
     @units = []
+    @renter_names = []
   end
 
   def add_unit(unit)
     @units << unit
+  end
+
+  def renters
+    units.each do |unit|
+      @renter_names << unit.renter.name
+    end
+    @renter_names
   end
 end

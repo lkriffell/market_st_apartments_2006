2d663b120dfd19a7e8de14da1c0190d919ac6e7c
Logan Riffell <lkriffell@gmail.com>
2020-07-14 10:39:17 -0600

    Add units by bedrooms passes test

--- lib/building.rb
+++ lib/building.rb
@@ -27,12 +27,31 @@ class Building
   end
 
   def rented_units
-    units_rented = []
+    units.find_all do |unit|
+      unit.renter != nil
+    end
+  end
+
+  # def renter_with_highest_rent
+  #   previous = 0
+  #   rented_units.each do |unit|
+  #     require "pry"; binding.pry
+  #     if unit.monthly_rent > previous
+  #       previous == unit.monthly_rent
+  #     end
+  #   end
+  # end
+
+  def units_by_number_of_bedrooms
+    units_by_beds = {}
     units.each do |unit|
-      if unit.renter != nil
-        units_rented << unit
+      if units_by_beds[unit.bedrooms] == nil
+        units_by_beds[unit.bedrooms] = [unit.number]
+      else
+        units_by_beds[unit.bedrooms] << unit.number
       end
     end
-    units_rented
+    require "pry"; binding.pry
+    units_by_beds.reverse
   end
 end

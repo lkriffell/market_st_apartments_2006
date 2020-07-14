8cfcb8f83a83d320cc2526091fb8ef11f63b2fd2
Logan Riffell <lkriffell@gmail.com>
2020-07-14 10:33:47 -0600

    Add units_by_bedrooms test

--- test/building_test.rb
+++ test/building_test.rb
@@ -55,9 +55,30 @@ class BuildingTest < Minitest::Test
     unit1.add_renter(renter2)
     renter3 = Renter.new("Max")
     unit3.add_renter(renter3)
+    building.renter_with_highest_rent
 
-    assert_equal [unit1], building.renter_with_highest_rent
+    assert_equal [unit1.renter], building.renter_with_highest_rent
   end
 
+  def test_units_by_number_of_bedrooms
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    unit2.add_renter(renter1)
+    renter2 = Renter.new("Jessie")
+    unit1.add_renter(renter2)
+    renter3 = Renter.new("Max")
+    unit3.add_renter(renter3)
+    building.add_unit(unit4)
+    units_by_beds = {3 => ["D4" ], 2 => ["B2", "C3"], 1 => ["A1"]}
+
+    assert_equal units_by_beds, building.units_by_number_of_bedrooms
+  end
 
 end
